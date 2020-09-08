dcf0671fd6f2f553b7c659a3d735cfca4f0e870d
马过 <mmmaaaggg@163.com>
2020-09-08 21:01:39 +0800

    feat: move functions to common.py

--- db/pythonapi/load_tick_csv.py
+++ db/pythonapi/load_tick_csv.py
@@ -39,12 +39,10 @@ PreClosePrice (昨收盘),
 PreOpenInterest (昨持仓),
 PreSettlementPrice (上次结算价)
 """
-import math
 import os
 import csv
-import re
 import logging
-from datetime import datetime, time, timedelta
+from datetime import datetime, time
 import pandas as pd
 import numpy as np
 import config as _config  # NOQA
@@ -52,46 +50,10 @@ from vnpy.trader.constant import Exchange, Interval
 from vnpy.trader.database import database_manager
 from vnpy.trader.object import TickData, BarData
 
+from db.common import get_file_iter, INSTRUMENT_EXCHANGE_DIC, PATTERN_INSTRUMENT_TYPE, merge_df_2_minutes_bar
+
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-PATTERN_INSTRUMENT_TYPE = re.compile(r'[A-Za-z]+(?=\d+$)')
-# 交易所品种对照表参考链接
-# http://www.khqihuo.com/spqh/1443.html
-EXCHANGE_INSTRUMENTS_DIC = {
-    Exchange.CFFEX: ("IF", "IC", "IH", "TS", "TF", "T"),
-    Exchange.SHFE: ("CU", "AL", "ZN", "PB", "RU", "AU",
-                    "FU", "RB", "WR", "AG", "BU", "HC",
-                    "NI", "SN", "SP", "SS"),
-    Exchange.CZCE: ("WH", "PM", "CF", "SR", "AT", "OI",
-                    "RS", "RM", "RI", "FG", "ZC", "JR",
-                    "MA", "LR", "SF", "SM", "CY", "AP",
-                    "CJ", "UR"),
-    Exchange.DCE: ("A", "B", "M", "Y", "C", "CS", "L",
-                   "P", "V", "J", "JM", "I", "JD", "FB",
-                   "BB", "PP", "RR", "EB", "EG"),
-    Exchange.INE: ("SC", "NR"),
-}
-
-INSTRUMENT_EXCHANGE_DIC = {}
-for _exchange, _inst_list in EXCHANGE_INSTRUMENTS_DIC.items():
-    for _inst in _inst_list:
-        INSTRUMENT_EXCHANGE_DIC[_inst] = _exchange
-
-
-def get_file_iter(folder_path, filters=None):
-    """
-    文件列表迭代器，返回当前目录及子目录下的所有文件名及文件路径
-    :param folder_path:
-    :param filters: 文件后缀名筛选
-    :return:
-    """
-    for file_name in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, file_name)
-        if os.path.isdir(file_path):
-            yield from get_file_iter(file_path)
-        else:
-            if filters is None or os.path.splitext(file_name)[1] in filters:
-                yield file_name, file_path
 
 
 def run_load_csv(folder_path=os.path.curdir):
@@ -101,30 +63,6 @@ def run_load_csv(folder_path=os.path.curdir):
     for file_name, file_path in get_file_iter(folder_path, filters=[".csv"]):
         logger.info("载入文件：%s", file_path)
         load_csv(file_path)
-
-
-def generate_bar_dt(dt, minutes):
-    date_only = datetime(dt.year, dt.month, dt.day)
-    delta = dt.to_pydatetime() - date_only
-    key = math.ceil(delta.seconds / 60 / minutes)
-    by = date_only + timedelta(minutes=key * minutes)
-    return by
-
-
-def merge_df_2_minutes_bar(df: pd.DataFrame, minutes: int) -> pd.DataFrame:
-    data_list = []
-    for bar_dt, sub_df in df.groupby(df['datetime'].apply(lambda x: generate_bar_dt(x, minutes))):
-        data_list.append({
-            'datetime': bar_dt,
-            'open_interest': sub_df['open_interest'].iloc[-1],
-            'open_price': sub_df['open_price'].iloc[0],
-            'high_price': sub_df['high_price'].max(),
-            'low_price': sub_df['low_price'].min(),
-            'close_price': sub_df['close_price'].iloc[-1],
-            'volume': sub_df['volume'].sum(),  # 目前没有 volume
-        })
-    new_df = pd.DataFrame(data_list)
-    return new_df
 
 
 def load_csv(file_path):
