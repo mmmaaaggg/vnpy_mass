f2ba64e6f229dbfd3e93cda3bb4eaf26b12be749
mmmaaaggg <mmmaaaggg@163.com>
2020-09-11 07:18:13 +0800

    feat: add add_unique_index_on_tick_data， add_unique_index_on_bar_data

--- db/utils.py
+++ db/utils.py
@@ -33,5 +33,33 @@ SUBPARTITION BY KEY(symbol) SUBPARTITIONS 100
     # meta.database.execute(sql_str)
 
 
+def add_unique_index_on_bar_data():
+    """
+    批量导入的时候会删除索引，导入数据完后再重建索引，以提高导入速度
+    当前函数用于重建 dbbardata 索引
+    :return:
+    """
+    sql_str = """ALTER TABLE `dbbardata` 
+ADD UNIQUE INDEX `dbbardata_symbol_exchange_interval_datetime` (`symbol` ASC, `exchange` ASC, `interval` ASC, `datetime` ASC) VISIBLE
+"""
+    # 手动执行sql 语句
+    meta = database_manager.class_bar._meta
+    meta.database.execute(sql_str)
+
+
+def add_unique_index_on_tick_data():
+    """
+    批量导入的时候会删除索引，导入数据完后再重建索引，以提高导入速度
+    当前函数用于重建 dbtickdata 索引
+    :return:
+    """
+    sql_str = """ALTER TABLE `dbtickdata` 
+ADD UNIQUE INDEX `dbtickdata_symbol_exchange_datetime` (`symbol` ASC, `exchange` ASC, `datetime` ASC) VISIBLE
+"""
+    # 手动执行sql 语句
+    meta = database_manager.class_bar._meta
+    meta.database.execute(sql_str)
+
+
 if __name__ == "__main__":
     pass
