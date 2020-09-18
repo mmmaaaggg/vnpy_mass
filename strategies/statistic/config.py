72f0adbff26384c2b47eb2c87c9ffdcd628a3e37
mmmaaaggg <mmmaaaggg@163.com>
2020-09-18 11:07:57 +0800

    feat: config.py 配置文件，近配合测试使用

---
+++ strategies/statistic/config.py
@@ -0,0 +1,9 @@
+"""
+@author  : MG
+@Time    : 2020/9/2 16:58
+@File    : config.py
+@contact : mmmaaaggg@163.com
+@desc    : 用于配置文件，由于vnpy运行时之家载 strategies 目录，
+因此，将config文件复制一份到这个目录下
+"""
+from config import *  # NOQA
